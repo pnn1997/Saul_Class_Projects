35bc519c461b67331f1ebea77a771a861cfd17e3
pnn1997 <pnn1997@gmail.com>
2016-11-05 15:17:24 -0500

    Updating files in NathanData folder

--- src/main/java/Readers/NathanNguyen/NathanImageReader.java
+++ src/main/java/Readers/NathanNguyen/NathanImageReader.java
@@ -4,34 +4,53 @@ package Readers.NathanNguyen;
  * Created by Nathan on 10/4/2016.
  */
 
+/* Previous import for processing the images. No longer applicable
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+*/
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NathanImageReader {
-    //imgs stores a list of image data obtained from image processing the different images
-    public List<ImageData> imgs;
+    //images stores a list of the image file names
+    public List<String> images;
 
-    //Takes in String dataFile with the file names of each image and iterates through and processes all
-    // the images in listed by the file and stores them in imgs
+    //Takes in String dataFile with the file names of each image and stores them in images
     public NathanImageReader(String dataFile) {
-        imgs = new ArrayList<ImageData>();
+        //Stores the list of the names of the image file
+        images = new ArrayList<String>();
 
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(dataFile)));
 
+            String str;
+            while ((str = br.readLine()) != null) {
+                images.add(str);
+            }
+
+            br.close();
+        }catch (Exception e) {}
+
+        /* Previous code for processing the incoming images. Current strategy is to store the list of image files and process them elsewhere
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(dataFile)));
+
+            //Stores the buffered image
             BufferedImage img = null;
             String str;
 
             while ((str = br.readLine()) != null) {
                 try {
                     img = ImageIO.read(new File(str));
-                    imgs.add(ImageProcess.edgeDetec(img));
+                    images.add(" ");
                 } catch (IOException e) {}
             }
 
             br.close();
             new FileInputStream(dataFile);
         } catch (Exception e) {}
+        */
+
     }
 }
